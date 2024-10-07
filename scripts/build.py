a5aea5318d50266f4968785ee9ec606512ca29f3
retrobanner <live@goml.ru>
2024-10-07 22:41:39 +0300

    feat: add new parser into scripts, update build-conf.toml with all settings,
    \fix: fix typo in example/markdown/work.md change ## to ###

--- scripts/build.py
+++ scripts/build.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 import tomllib
-import yaml
+from parser import write_yaml
 
 # Change the current working directory to the root of the project
 os.chdir(os.path.join(os.path.dirname(__file__), ".."))
@@ -18,67 +18,7 @@ os.makedirs("tmp", exist_ok=True)
 with open("build-conf.toml", "rb") as file:
     conf = tomllib.load(file)
 
-with open("tmp/metadata.yaml", "w") as file:
-    file.write(
-        yaml.dump(
-            {
-                "title": conf["metadata"]["title"],
-                "subtitle": conf["metadata"]["subtitle"],
-                "author": conf["metadata"]["author"],
-                "date": conf["metadata"]["date"],
-                "geometry": conf["metadata"]["format"]["geometry"],
-                "papersize": conf["metadata"]["format"]["papersize"],
-                "lang": conf["metadata"]["lang"],
-                "linkcolor": conf["metadata"]["format"]["linkcolor"],
-                "toc-own-page": conf["metadata"]["format"]["toc-own-page"],
-                "titlepage": conf["metadata"]["title-page"]["title-page"],
-                "titlepage-background": (
-                    os.path.join(
-                        "assets",
-                        "title-page",
-                        conf["metadata"]["title-page"]["background"],
-                    )
-                    if "background" in conf["metadata"]["title-page"]
-                    else None
-                ),
-                "titlepage-logo": (
-                    os.path.join(
-                        "assets",
-                        "title-page",
-                        conf["metadata"]["title-page"]["logo"],
-                    )
-                    if "logo" in conf["metadata"]["title-page"]
-                    else None
-                ),
-                "logo-width": (
-                    conf["metadata"]["title-page"]["logo-width"]
-                    if "logo-width" in conf["metadata"]["title-page"]
-                    else None
-                ),
-                "titlepage-rule-height": conf["metadata"]["title-page"]["rule-height"],
-                "disable-header-and-footer": conf["metadata"]["format"][
-                    "disable-header-and-footer"
-                ],
-                "block-headings": conf["metadata"]["format"]["block-headings"],
-                "listings-no-page-break": conf["metadata"]["format"][
-                    "listings-no-page-break"
-                ],
-                "caption-justification": conf["metadata"]["format"][
-                    "caption-justification"
-                ],
-                "footnotes-pretty": conf["metadata"]["format"]["footnotes-pretty"],
-                "book": conf["metadata"]["format"]["book"],
-                "header-right": conf["metadata"]["format"]["header"]["right"],
-                "header-left": conf["metadata"]["format"]["header"]["left"],
-                "footer-left": conf["metadata"]["format"]["footer"]["left"],
-                "footer-right": conf["metadata"]["format"]["footer"]["right"],
-                "biblio-style": conf["metadata"]["latex"]["biblio-style"],
-            },
-            allow_unicode=True,
-            sort_keys=False,
-        )
-    )
-    file.write(conf["metadata"]["latex"]["header-includes"])
+write_yaml(conf, "tmp/metadata.yaml")
 
 files = [os.path.join("src", file) for file in conf["pandoc"]["src"]]
 
