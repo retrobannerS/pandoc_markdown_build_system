2df84f34e1caa4178ea6567dd7ae9ffd51c08c0b
retrobanner <live@goml.ru>
2024-10-08 22:39:28 +0300

    feat: add img optimizer, add separation on latex and markdown project, add latex template parser, add preambles support

--- scripts/build.py
+++ scripts/build.py
@@ -1,15 +1,15 @@
 import os
 import shutil
 import tomllib
-from parser import write_yaml
+from parser import write_yaml, write_template
+from image_optimizer import optimize_images
 
 # Change the current working directory to the root of the project
 os.chdir(os.path.join(os.path.dirname(__file__), ".."))
 
-# Ensure ./out exists and is empty
+# Recursively remove out directory and then create it again
+shutil.rmtree("out", ignore_errors=True)
 os.makedirs("out", exist_ok=True)
-for filename in os.listdir("out"):
-    os.remove(os.path.join("out", filename))
 
 # Recursively remove temporary directory and then create it again
 shutil.rmtree("tmp", ignore_errors=True)
@@ -20,12 +20,55 @@ with open("build-conf.toml", "rb") as file:
 
 write_yaml(conf, "tmp/metadata.yaml")
 
-files = [os.path.join("src", file) for file in conf["pandoc"]["src"]]
+# Optimize images
+if conf["image-optimization"]["enabled"]:
+    optimize_images(
+        "assets/images",
+        "tmp/images",
+        conf["image-optimization"]["quality"],
+        conf["image-optimization"]["max-size"],
+    )
 
-# Convert all markdown files in the src directory to a single PDF
-os.system(
-    f"pandoc {' '.join(files)} -o out/{conf['pandoc']['out-name']} {' '.join(conf['pandoc']['args'])}"
-)
+if conf["project"]["latex"]:  # if latex project
+    # Create void file and link out template
+    with open("tmp/void.tex", "w") as file:
+        pass
+    os.system(
+        f"pandoc tmp/void.tex -o tmp/raw_template.tex {' '.join(conf['pandoc']['args'])}"
+    )
+
+    # Parse our template
+    preambles = [
+        os.path.join("preambles", file) for file in conf["project"]["preambles"]
+    ]
+    write_template(preambles)
+
+    # Move to separated folder
+    shutil.rmtree("src/template", ignore_errors=True)
+    os.makedirs("src/template", exist_ok=True)
+    shutil.move("tmp/template.tex", "src/template/template.tex")
+    shutil.move("tmp/titlepage.tex", "src/template/titlepage.tex")
+    shutil.move("tmp/toc.tex", "src/template/toc.tex")
+
+    # create main.tex if is does not exist
+    if os.path.exists("src/main.tex"):
+        None
+    else:
+        with open("src/main.tex", "w") as main:
+            main.write("\\input{template/template.tex}\n\n")
+            main.write("\\begin{document}\n")
+            main.write("\t\\input{template/titlepage.tex}\n")
+            main.write("\t\\input{template/toc.tex}\n")
+            main.write("\t\n\t\n\t\n")
+            main.write("\\end{document}\n")
+
+else:  # if markdown project
+    files = [os.path.join("src", file) for file in conf["pandoc"]["src"]]
+
+    # Convert all markdown files in the src directory to a single PDF
+    os.system(
+        f"pandoc {' '.join(files)} -o out/{conf['pandoc']['out-name']} {' '.join(conf['pandoc']['args'])}"
+    )
 
 
 # Remove temporary directory
---
+++ scripts/image_optimizer.py
@@ -0,0 +1,32 @@
+import os
+from PIL import Image
+
+
+def optimize_images(input_dir, output_dir, quality, max_size):
+    print(f"Optimizing images from {input_dir} to {output_dir} with quality {quality}")
+    for root, dirs, files in os.walk(input_dir):
+        for file in files:
+            file_path = os.path.join(root, file)
+            optimize_image(input_dir, file_path, output_dir, quality, max_size)
+
+
+def optimize_image(input_dir, file_path, output_dir, quality, max_size):
+    try:
+        img = Image.open(file_path)
+        img = fill_transparent(img)
+        img.thumbnail((max_size, max_size))
+        img = img.convert("RGB")
+        new_file_path = file_path.replace(input_dir, output_dir)
+        new_file_path = os.path.splitext(new_file_path)[0] + ".jpg"
+        new_file_dir = os.path.dirname(new_file_path)
+        os.makedirs(new_file_dir, exist_ok=True)
+        img.save(new_file_path, "JPEG", optimize=True, quality=quality)
+    except Exception as e:
+        print(f"Error optimizing {file_path}: {e}")
+
+
+def fill_transparent(img):
+    non_transparent = Image.new("RGB", img.size, (255, 255, 255))
+    non_transparent.paste(img, (0, 0), img)
+    return non_transparent
+
